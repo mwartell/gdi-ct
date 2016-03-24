ce8246f5a02eca6b6f746cfbc46ab61c22aeeaa5
matt wartell <matt.wartell@gmail.com>
2016-03-24 19:21:39 -0400

    draft presentation complete

--- count2.py
+++ count2.py
@@ -12,5 +12,20 @@ def ice_cream_counts(path):
 
     return counts
 
+heading = """
+<!DOCTYPE html>
+<html><head><title>Ice Cream Preferences</title></head><body>
+<table border="1">
+<tr> <th>preference</th> <th>female</th> <th>male</th> </tr>""".lstrip()
+table_row = "<tr> <td>{}</td> <td>{}</td> <td>{}</td> </tr>"
+closing = "</table></body></html>"
+
+def write_html(counts):
+    print(heading)
+    for flavor in ['strawberry', 'pistachio']:
+        p = flavor[0]
+        print(table_row.format(flavor, counts['f'][p], counts['m'][p]))
+    print(closing)
+
 counts = ice_cream_counts('ice-cream.csv')
-print(counts)
+write_html(counts)
